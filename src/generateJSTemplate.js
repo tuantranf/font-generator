b90e6ed7258c68b9b53c2fe18ffe726ea46f93aa
Tran Minh Tuan <tuantranf@gmail.com>
2018-07-09 22:12:17 +0700

    :+1: format icon name as camel case

--- src/generateJSTemplate.js
+++ src/generateJSTemplate.js
@@ -2,6 +2,8 @@ const isAndroid = (name) => name.indexOf('android') > -1;
 
 const lowercaseFirstLetter = text => `${text.charAt(0).toLowerCase()}${text.slice(1)}`;
 
+const snakeToCamel = text => text.replace(/(\-\w)/g, (m) => m[1].toUpperCase());
+
 const withoutPlatform = (name) => lowercaseFirstLetter(name.replace('ios', '').replace('android', ''));
 
 /**
@@ -12,14 +14,14 @@ const getJsTemplateData = (codepoints) => {
   const ios = Object.keys(codepoints)
     .filter((name) => name.indexOf('ios') > -1)
     .map((name) => ({
-      name: withoutPlatform(name),
+      name: snakeToCamel(withoutPlatform(name)),
       value: codepoints[name].toString(16),
   }));
 
   const android = Object.keys(codepoints)
     .filter((name) => name.indexOf('android') > -1)
     .map((name) => ({
-      name: withoutPlatform(name),
+      name: snakeToCamel(withoutPlatform(name)),
       value: codepoints[name].toString(16),
     }));
 
